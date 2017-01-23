be549a72d0a9b78192437dfc5ddabd2e74a10d7a
Bogdan Vaskan <fant1k450@gmail.com>
2017-01-23 19:45:29 +0200

    поправил функции

--- js/stat.js
+++ js/stat.js
@@ -1,30 +1,32 @@
 'use strict';
 
-var drawCloud = function (ctx, x, y, width, heigth) {
-  var offset = 10;
-  ctx.beginPath();
-  ctx.moveTo(x, y);
-  ctx.lineTo(x + offset, y + heigth / 2);
-  ctx.lineTo(x, y + heigth);
-  ctx.lineTo(x + width / 2, y + heigth - offset);
-  ctx.lineTo(x + width, y + heigth);
-  ctx.lineTo(x + width - offset, y + heigth / 2);
-  ctx.lineTo(x + width, y);
-  ctx.lineTo(x + width / 2, y + offset);
-  ctx.lineTo(x, y);
-  ctx.stroke();
-  ctx.closePath();
-  ctx.fill();
-};
-
-var printText = function (ctx, data, histoX, columnIndent, i, height) {
-  ctx.fillText(data, histoX + columnIndent * i, height);
-};
-var drawHisto = function (ctx, histoX, columnIndent, i, height) {
-  ctx.fillRect(histoX + columnIndent * i, 245 - height, 40, height);
-};
-
 window.renderStatistics = function (ctx, names, times) {
+
+  var drawCloud = function (x, y, width, heigth) {
+    var offset = 10;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + offset, y + heigth / 2);
+    ctx.lineTo(x, y + heigth);
+    ctx.lineTo(x + width / 2, y + heigth - offset);
+    ctx.lineTo(x + width, y + heigth);
+    ctx.lineTo(x + width - offset, y + heigth / 2);
+    ctx.lineTo(x + width, y);
+    ctx.lineTo(x + width / 2, y + offset);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    ctx.closePath();
+    ctx.fill();
+  };
+  var printText = function (data, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillText(data, width, height);
+  };
+
+  var drawHisto = function (x1, y1, x2, y2) {
+    ctx.fillRect(x1, y1, x2, y2);
+  };
+
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   drawCloud(ctx, 110, 20, 420, 270);
 
@@ -61,9 +63,8 @@ window.renderStatistics = function (ctx, names, times) {
     } else {
       ctx.fillStyle = ['rgb(0, 0,', ((Math.random() * 5) * 50).toFixed(0), ')'].join('');
     }
-    drawHisto(ctx, histoX, columnIndent, i, height);
-    ctx.fillStyle = '#000';
-    printText(ctx, time.toFixed(0), histoX, columnIndent, i, 240 - height);
-    printText(ctx, name, histoX, columnIndent, i, 260);
+    drawHisto((histoX + columnIndent * i), (245 - height), 40, height);
+    printText(time.toFixed(0), (histoX + columnIndent * i), (240 - height), '#000');
+    printText(name, (histoX + columnIndent * i), 260, '#000');
   }
 };

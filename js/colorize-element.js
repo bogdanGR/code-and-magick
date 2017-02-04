021b24e7bd686e391dd1a35f2a3ae28031d1fde4
Bogdan Vaskan <fant1k450@gmail.com>
2017-02-04 13:04:48 +0200

    Разделил код на модули

---
+++ js/colorize-element.js
@@ -0,0 +1,33 @@
+'use strict';
+
+window.colorizeElement = function (element, colors, property) {
+  var getRandomColor = function () {
+    var currentColor = window.utils.getRandomElementExcept(colors, rgbToHex(element.style[property]));
+    element.style[property] = currentColor;
+    window.changeAria(element);
+  };
+  var getRandomColorByKeyborad = function (evt) {
+    if (window.isActivateEvent(evt)) {
+      getRandomColor();
+      window.changeAria(element);
+    }
+  };
+  var rgbToHex = function (color) {
+    var nums = /(.*?)rgb\((\d+),\s*(\d+),\s*(\d+)\)/i.exec(color);
+    if (!nums) {
+      return color;
+    }
+    var r = parseInt(nums[2], 10).toString(16);
+    var g = parseInt(nums[3], 10).toString(16);
+    var b = parseInt(nums[4], 10).toString(16);
+
+    return '#' + (
+      (r.length === 1 ? '0' + r : r) +
+      (g.length === 1 ? '0' + g : g) +
+      (b.length === 1 ? '0' + b : b)
+    );
+  };
+
+  element.addEventListener('keydown', getRandomColorByKeyborad);
+  element.addEventListener('click', getRandomColor);
+};
---
+++ js/utils.js
@@ -0,0 +1,16 @@
+'use strict';
+
+window.utils = {
+  getRandomElement: function (array) {
+    var randomElementIndex = Math.floor(Math.random() * array.length);
+    return array[randomElementIndex];
+  },
+  getRandomElementExcept: function (arr, arrElement) {
+    var currentColor = this.getRandomElement(arr);
+
+    while (currentColor === arrElement) {
+      currentColor = this.getRandomElement(arr);
+    }
+    return currentColor;
+  }
+};

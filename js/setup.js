d74d98057a63f5194980aca0edc8d7ab10e8fc46
Bogdan Vaskan <fant1k450@gmail.com>
2017-02-08 10:11:31 +0200

    Инкапсулировал!

--- js/setup.js
+++ js/setup.js
@@ -1,58 +1,70 @@
 'use strict';
 
-var nameField = document.querySelector('.setup-user-name');
-var setup = document.querySelector('.setup');
-var setupOpen = document.querySelector('.setup-open');
-var setupClose = setup.querySelector('.setup-close');
+window.setup = (function () {
 
-var ESCAPE_KEY_CODE = 27;
-var ENTER_KEY_CODE = 13;
+  var ENTER_KEY_CODE = 13;
 
-var changeAria = function (element) {
+  return {
+    changeAria: function (element) {
 
-  var pressed = (element.getAttribute('aria-pressed') === 'true');
-  if (!pressed) {
-    element.setAttribute('aria-pressed', !pressed);
-  }
-};
-var isActivateEvent = function (evt) {
-  return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
-};
-var setupKeydownHandler = function (evt) {
-  if (evt.keyCode === ESCAPE_KEY_CODE) {
+      var pressed = (element.getAttribute('aria-pressed') === 'true');
+      if (!pressed) {
+        element.setAttribute('aria-pressed', !pressed);
+      }
+    },
+    isActivateEvent: function (evt) {
+      return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
+    }
+  };
+})();
+
+(function () {
+  var nameField = document.querySelector('.setup-user-name');
+  var setup = document.querySelector('.setup');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = setup.querySelector('.setup-close');
+
+  var ESCAPE_KEY_CODE = 27;
+
+  var setupKeydownHandler = function (evt) {
+    if (evt.keyCode === ESCAPE_KEY_CODE) {
+      setup.classList.add('invisible');
+    }
+  };
+  var showSetupElement = function () {
+    setup.classList.remove('invisible');
+    document.addEventListener('keydown', setupKeydownHandler);
+  };
+  var hideSetupElement = function () {
     setup.classList.add('invisible');
-  }
-};
-var showSetupElement = function () {
-  setup.classList.remove('invisible');
-  document.addEventListener('keydown', setupKeydownHandler);
-};
-var hideSetupElement = function () {
-  setup.classList.add('invisible');
-  document.removeEventListener('keydown', setupKeydownHandler);
-};
-setupOpen.addEventListener('click', function () {
-  showSetupElement();
-  changeAria(setupOpen);
-});
-setupOpen.addEventListener('keydown', function (evt) {
-  if (isActivateEvent(evt)) {
+    document.removeEventListener('keydown', setupKeydownHandler);
+  };
+  setupOpen.addEventListener('click', function () {
     showSetupElement();
-    changeAria(setupOpen);
-  }
-});
-setupClose.addEventListener('click', function () {
-  hideSetupElement();
-  changeAria(setupClose);
-});
-setupClose.addEventListener('keydown', function (evt) {
-  if (isActivateEvent(evt)) {
+    window.setup.changeAria(setupOpen);
+  });
+  setupOpen.addEventListener('keydown', function (evt) {
+    if (window.setup.isActivateEvent(evt)) {
+      showSetupElement();
+      window.setup.changeAria(setupOpen);
+    }
+  });
+  setupClose.addEventListener('click', function () {
     hideSetupElement();
-    changeAria(setupClose);
-  }
-});
+    window.setup.changeAria(setupClose);
+  });
+  setupClose.addEventListener('keydown', function (evt) {
+    if (window.setup.isActivateEvent(evt)) {
+      hideSetupElement();
+      window.changeAria(setupClose);
+    }
+  });
+  nameField.required = true;
+  nameField.maxLength = 50;
+})();
 
-window.colorizeElement(document.querySelector('#wizard-coat'),
+
+window.getColorElement.colorizeElement(document.querySelector('#wizard-coat'),
   ['#ee4830',
     '#30a8ee',
     '#5ce6c0',
@@ -60,7 +72,7 @@ window.colorizeElement(document.querySelector('#wizard-coat'),
     '#e6e848'],
     'fill'
  );
-window.colorizeElement(document.querySelector('.setup-fireball-wrap'),
+window.getColorElement.colorizeElement(document.querySelector('.setup-fireball-wrap'),
   ['#ee4830',
     '#30a8ee',
     '#5ce6c0',
@@ -68,7 +80,7 @@ window.colorizeElement(document.querySelector('.setup-fireball-wrap'),
     '#e6e848'],
     'backgroundColor'
   );
-window.colorizeElement(document.querySelector('#wizard-eyes'),
+window.getColorElement.colorizeElement(document.querySelector('#wizard-eyes'),
   ['black',
     'red',
     'blue',
@@ -76,7 +88,3 @@ window.colorizeElement(document.querySelector('#wizard-eyes'),
     'green'],
     'fill'
    );
-
-
-nameField.required = true;
-nameField.maxLength = 50;

146d5359f8392b782ae2a4e0a61f3d43eaf20734
Bogdan Vaskan <fant1k450@gmail.com>
2017-02-09 14:53:58 +0200

    Позвонил!

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,37 +1,26 @@
 'use strict';
-window.getColorElement = (function () {
+window.getColorElement = function (element, colors, property, callback) {
 
-  return {
-    colorizeElement: function (element, colors, property) {
-      var getRandomColor = function () {
-        var currentColor = window.utils.getRandomElementExcept(colors, rgbToHex(element.style[property]));
-        element.style[property] = currentColor;
-        window.changeAria(element);
-      };
-      var getRandomColorByKeyborad = function (evt) {
-        if (window.isActivateEvent(evt)) {
-          getRandomColor();
-          window.changeAria(element);
-        }
-      };
-      var rgbToHex = function (color) {
-        var nums = /(.*?)rgb\((\d+),\s*(\d+),\s*(\d+)\)/i.exec(color);
-        if (!nums) {
-          return color;
-        }
-        var r = parseInt(nums[2], 10).toString(16);
-        var g = parseInt(nums[3], 10).toString(16);
-        var b = parseInt(nums[4], 10).toString(16);
+  var getRandomColor = function () {
 
-        return '#' + (
+    return window.utils.getRandomElementExcept(colors, rgbToHex(element.style[property]));
+  };
+
+  var rgbToHex = function (color) {
+    var nums = /(.*?)rgb\((\d+),\s*(\d+),\s*(\d+)\)/i.exec(color);
+    if (!nums) {
+      return color;
+    }
+    var r = parseInt(nums[2], 10).toString(16);
+    var g = parseInt(nums[3], 10).toString(16);
+    var b = parseInt(nums[4], 10).toString(16);
+
+    return '#' + (
           (r.length === 1 ? '0' + r : r) +
           (g.length === 1 ? '0' + g : g) +
           (b.length === 1 ? '0' + b : b)
         );
-      };
-
-      element.addEventListener('keydown', getRandomColorByKeyborad);
-      element.addEventListener('click', getRandomColor);
-    }
   };
-})();
+
+  return callback(getRandomColor(), property);
+};
--- js/setup.js
+++ js/setup.js
@@ -1,90 +1,160 @@
 'use strict';
 
-window.setup = (function () {
-
+window.settings = (function () {
   var ENTER_KEY_CODE = 13;
+  var ESCAPE_KEY_CODE = 27;
+
+  var isKeyboardEvent = function (evt) {
+    return typeof evt.keyCode !== 'undefined';
+  };
 
   return {
-    changeAria: function (element) {
-
-      var pressed = (element.getAttribute('aria-pressed') === 'true');
-      if (!pressed) {
-        element.setAttribute('aria-pressed', !pressed);
-      }
+    isDeactivationEvent: function (evt) {
+      return isKeyboardEvent(evt) && evt.keyCode === ESCAPE_KEY_CODE;
     },
-    isActivateEvent: function (evt) {
-      return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
-    }
+
+    isActivationEvent: function (evt) {
+      return isKeyboardEvent(evt) && evt.keyCode === ENTER_KEY_CODE;
+    },
+
+    isKeyboardEvent: isKeyboardEvent
   };
 })();
 
-(function () {
-  var nameField = document.querySelector('.setup-user-name');
+window.enableSetup = (function () {
   var setup = document.querySelector('.setup');
-  var setupOpen = document.querySelector('.setup-open');
+  var nameField = document.querySelector('.setup-user-name');
   var setupClose = setup.querySelector('.setup-close');
+  var onSetupClose = null;
 
-  var ESCAPE_KEY_CODE = 27;
-
-  var setupKeydownHandler = function (evt) {
-    if (evt.keyCode === ESCAPE_KEY_CODE) {
-      setup.classList.add('invisible');
-    }
-  };
-  var showSetupElement = function () {
+  var openSetup = function () {
     setup.classList.remove('invisible');
     document.addEventListener('keydown', setupKeydownHandler);
   };
-  var hideSetupElement = function () {
+  var setupKeydownHandler = function (evt) {
+    if (window.settings.isDeactivationEvent(evt)) {
+      setup.classList.add('invisible');
+    }
+  };
+
+  var closeSetup = function () {
     setup.classList.add('invisible');
     document.removeEventListener('keydown', setupKeydownHandler);
-  };
-  setupOpen.addEventListener('click', function () {
-    showSetupElement();
-    window.setup.changeAria(setupOpen);
-  });
-  setupOpen.addEventListener('keydown', function (evt) {
-    if (window.setup.isActivateEvent(evt)) {
-      showSetupElement();
-      window.setup.changeAria(setupOpen);
+
+    if (typeof onSetupClose === 'function') {
+      onSetupClose();
     }
-  });
+  };
+
+  var onKeyDown = function (evt) {
+    if (window.settings.isActivationEvent(evt)) {
+      closeSetup();
+    }
+  };
   setupClose.addEventListener('click', function () {
-    hideSetupElement();
-    window.setup.changeAria(setupClose);
+    closeSetup();
+    window.settings.changeAria(setupClose);
   });
   setupClose.addEventListener('keydown', function (evt) {
-    if (window.setup.isActivateEvent(evt)) {
-      hideSetupElement();
-      window.changeAria(setupClose);
+    if (window.settings.isActivationEvent(evt)) {
+      closeSetup();
+      window.settings.changeAria(setupClose);
     }
   });
   nameField.required = true;
   nameField.maxLength = 50;
+
+  return function (cb) {
+    openSetup();
+    setupClose.addEventListener('keydown', onKeyDown);
+
+    onSetupClose = cb;
+  };
+
 })();
 
+var coatNode = document.querySelector('#wizard-coat');
+var fireBallNode = document.querySelector('.setup-fireball-wrap');
+var eyesNode = document.querySelector('#wizard-eyes');
 
-window.getColorElement.colorizeElement(document.querySelector('#wizard-coat'),
-  ['#ee4830',
-    '#30a8ee',
-    '#5ce6c0',
-    '#e848d5',
-    '#e6e848'],
-    'fill'
- );
-window.getColorElement.colorizeElement(document.querySelector('.setup-fireball-wrap'),
-  ['#ee4830',
-    '#30a8ee',
-    '#5ce6c0',
-    '#e848d5',
-    '#e6e848'],
-    'backgroundColor'
+coatNode.addEventListener('keydown', function () {
+  window.getColorElement(coatNode,
+    ['#ee4830',
+      '#30a8ee',
+      '#5ce6c0',
+      '#e848d5',
+      '#e6e848'],
+      'fill',
+      function (colors, property) {
+        coatNode.style[property] = colors;
+      }
   );
-window.getColorElement.colorizeElement(document.querySelector('#wizard-eyes'),
-  ['black',
-    'red',
-    'blue',
-    'yellow',
-    'green'],
-    'fill'
-   );
+});
+coatNode.addEventListener('click', function () {
+  window.getColorElement(coatNode,
+    ['#ee4830',
+      '#30a8ee',
+      '#5ce6c0',
+      '#e848d5',
+      '#e6e848'],
+      'fill',
+      function (colors, property) {
+        coatNode.style[property] = colors;
+      }
+  );
+});
+
+
+fireBallNode.addEventListener('keydown', function () {
+  window.getColorElement(fireBallNode,
+    ['#ee4830',
+      '#30a8ee',
+      '#5ce6c0',
+      '#e848d5',
+      '#e6e848'],
+      'backgroundColor',
+      function (colors, property) {
+        fireBallNode.style[property] = colors;
+      }
+    );
+});
+fireBallNode.addEventListener('click', function () {
+  window.getColorElement(fireBallNode,
+    ['#ee4830',
+      '#30a8ee',
+      '#5ce6c0',
+      '#e848d5',
+      '#e6e848'],
+      'backgroundColor',
+      function (colors, property) {
+        fireBallNode.style[property] = colors;
+      }
+    );
+});
+
+eyesNode.addEventListener('keydown', function () {
+  window.getColorElement(eyesNode,
+    ['black',
+      'red',
+      'blue',
+      'yellow',
+      'green'],
+      'fill',
+      function (colors, property) {
+        eyesNode.style[property] = colors;
+      }
+    );
+});
+eyesNode.addEventListener('click', function () {
+  window.getColorElement(eyesNode,
+    ['black',
+      'red',
+      'blue',
+      'yellow',
+      'green'],
+      'fill',
+      function (colors, property) {
+        eyesNode.style[property] = colors;
+      }
+    );
+});
